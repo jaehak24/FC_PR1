33e5438204b2e27894920d069e431c7bc61e0e8d
jaehak24 <65396939+jaehak24@users.noreply.github.com>
2023-01-05 16:44:21 +0900

    # 11 Java 클래스로 도메인 설계

---
+++ Board_pr/src/main/java/com/example/board_pr/domain/Article.java
@@ -0,0 +1,21 @@
+package com.example.board_pr.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+
+    private String title;
+
+    private String hashtag;
+
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime modifiedAt;
+
+    private String modifiedBy;
+
+}
