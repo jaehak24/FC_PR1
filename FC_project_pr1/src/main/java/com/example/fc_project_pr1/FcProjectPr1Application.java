a159c943457ea18bad74ed8aedba51c3305fc0a1
jaehak24 <65396939+jaehak24@users.noreply.github.com>
2023-01-16 17:17:18 +0900

    adding project file
    adding Project file

---
+++ FC_project_pr1/src/main/java/com/example/fc_project_pr1/FcProjectPr1Application.java
@@ -0,0 +1,13 @@
+package com.example.fc_project_pr1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcProjectPr1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcProjectPr1Application.class, args);
+    }
+
+}
